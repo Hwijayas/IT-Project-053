33b633314df2667b837ec6f93300bf9b97a781b8
edward149 <edwardchia96@hotmail.sg>
2021-08-19 12:49:38 +0800

    set up web client

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,23 +1,55 @@
+//this entire chunk of code is from https://github.com/zeroabsolute/MonorepoHerokuDeployment/blob/master/src/web-client/src/App.js
+
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
 function App() {
+  const [apiStatus, setApiStatus] = useState("");
+
+  useEffect(() => {
+    Axios({
+      method: 'GET',
+      url: `${API_URL}/health`
+    }).then((response) => {
+      setApiStatus({ status: response.status, payload: response.data, error: null });
+    }).catch((error) => {
+      setApiStatus({ status: error.response?.status, payload: null, error: error.response?.data });
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>API Status:</h3>
+        {renderStatus(apiStatus)}
       </header>
+    </div>
+  );
+}
+
+function renderStatus(response) {
+  if (!response) {
+    return null;
+  } else if (response.error) {
+    return (
+      <div>
+        Code: {response.status} <br />
+        Message: {response.error?.message}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      Code: {response.status} <br />
+      Version: {response.payload?.version} <br />
+      Message: {response.payload?.status}
     </div>
   );
 }
