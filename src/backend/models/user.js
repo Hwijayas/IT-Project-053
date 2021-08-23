d5240df9a87d865f016d6ccd5aeb8a3dd2a4b3c6
edward149 <edwardchia96@hotmail.sg>
2021-08-23 12:08:05 +0800

    backend connects to db, added adduser func

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    first_name: String,
-    last_name: String,
+    userEmail: String,
+    userFirstName: String,
+    userLastName: String,
     userPw: String
 })
 
