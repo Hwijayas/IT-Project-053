cc062b40d920f516385eaace2e3477ac5649c3f0
drg89 <64565020+drg89@users.noreply.github.com>
2021-08-23 16:46:24 +1000

    working navBar

---
+++ src/client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '5vh'
+            }}
+        >
+            <h1>This is the CRM we created as a team!</h1>
+        </div>
+    );
+};
+
+export default About;
