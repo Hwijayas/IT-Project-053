5ed3fe5d4213b18971357813836baceadb4f9813
jawad <jmohammed@student.unimelb.edu.au>
2021-08-28 20:23:23 +1000

    Correcting the dependencies for passport jwt package

--- src/backend/routes/userRouter.js
+++ src/backend/routes/userRouter.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const passport = require('passport');
-const userController = require('../controllers/userController.js');
+const userController = require('../controllers/userController');
 
-router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-    res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.status(200).json({ success: true, msg: 'You are successfully authenticated to this route!' });
 });
 
 // Validate an existing user and issue a JWT
