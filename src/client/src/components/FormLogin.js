b42e14d6f8bee64f880f8901bfad43ecf3f3008d
HANS Wijaya <hwwij@student.unimelb.edu.au>
2021-08-31 02:24:29 +0700

    added logo, updated css and attempt login functionality

--- src/client/src/components/FormLogin.js
+++ src/client/src/components/FormLogin.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import useForm from './useForm'
-import validate from '../validateInfo';
-import "./Form.css"
+import validate from './validateInfo';
+import "../css/Form.css"
 
 const FormSignup = (submitForm) => {
     const{handleChange, values, handleSubmit, errors} = useForm(submitForm, validate);
--- src/client/src/components/pages/Form.js
+++ src/client/src/components/pages/Form.js
@@ -1,18 +1,38 @@
 import React, {useState} from 'react';
-import FormSignup from './FormSignup';
+import FormLogin from '../FormLogin';
 import "./Form.css"
+import Axios from 'axios';
+//import { response } from 'express';
 
 const Form = () => {
     const [isSubmitted, setIsSubmitted] = useState(false)
+    const [userEmail, setUserEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     function submitForm(){
         setIsSubmitted = (true);
     }
+
+    /*
+    const login = () => {
+        Axios.post("http://localhost5000/user/login",{ 
+            userEmail: userEmail,
+            password: password
+        }).then((response) => {
+            console.log(response);
+        });
+    }*/
+    
+
     return (
         <>   
         <div className='form-container'>
             {/* <span className='close-btn'>×</span> */}
-            {!isSubmitted ? <FormSignup submitForm={submitForm} /> : (<FormSuccess/>)}
+            <div className='form-content-left'>
+                <img className='form-img' src='BitsRMl_logo.svg' alt='logo' />
+            </div>
+            {/* {!isSubmitted ? <FormSignup submitForm={submitForm} /> : (<FormSuccess/>)} */}
+            <FormLogin submitForm={submitForm}/>
         </div>
         </>
     )
